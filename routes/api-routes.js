f398461b7bb638445d11539cfd3c927e263c3749
Kurt LaVacque <lavacquek@icloud.com>
2020-02-13 22:04:26 -0600

    Fixed some spacing in the routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,30 +9,25 @@ module.exports = function (app) {
     app.get("/api/workouts", async ({ body }, res) => {
         console.log("Complete")
         const request = await db.Workout.find({})
-            res.send(request)
+        res.send(request)
     });
 
     app.get("/api/workouts/range", async ({ body }, res) => {
         console.log("Range")
         const request = await db.Workout.find({}).populate("workout")
-            res.send(request)
-
-
+        res.send(request)
     });
 
     app.put("/api/workouts/:id", async (req, res) => {
         const request = await db.Workout.findOneAndUpdate({ _id: req.params.id }, {
-            $push: {exercises: req.body}
+            $push: { exercises: req.body }
         })
         res.send(request)
     });
 
     app.post("/api/workouts", async ({ body }, res) => {
-
         const request = await db.Workout.create(body)
-
         res.send(request)
-
     });
 
 };
