b2a9efef1589d3f224a1478a1be2cfd2f96433b0
Kurt LaVacque <lavacquek@icloud.com>
2020-02-14 07:41:07 -0600

    Added Comments to server.js

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
+// Import Express
 const express = require("express");
-// const logger = require("morgan");
+// Import Mongoose
 const mongoose = require("mongoose");
 
+// Set Port to Heroko Choice or 3000
 const PORT = process.env.PORT || 3000;
 
-// const db = require("./models/user");
-// const db = require("./models");
+// Assign Express to variable
 const app = express();
 
-// app.use(logger("dev"));
-
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Use the public folder
 app.use(express.static("public"));
 
+// Use mongodb database, so there is no need to use robo 3T
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
 
+// Import html routes
 require("./routes/html-routes.js")(app);
+// Import api routes
 require("./routes/api-routes.js")(app);
 
-
-
+// Listen to Port
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
