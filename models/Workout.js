f969d7fd8c075524f57fb3d8b4d923a6da7e8f31
Kurt LaVacque <lavacquek@icloud.com>
2020-02-14 07:31:08 -0600

    Commented on Workout.js Model

--- models/Workout.js
+++ models/Workout.js
@@ -1,18 +1,23 @@
+// Call Dependencies
 const mongoose = require("mongoose");
-
+// Assign Schema to a variable
 const Schema = mongoose.Schema;
-
+// Create a workout object maker
 const WorkoutSchema = new Schema({
+    // The current day
     day:
     {
         type: Date,
         default: Date.now()
     },
+    // The exercises
     exercises: [Object],
+    // The total duration the workouts have added up to
     totalDuration: Number
 
 });
 
+// Mongoose Compiles a model for you with the name "Workout" and with the model schema your provided
 const Workout = mongoose.model("Workout", WorkoutSchema);
-
+// Export your workout model to be used in your server
 module.exports = Workout;
